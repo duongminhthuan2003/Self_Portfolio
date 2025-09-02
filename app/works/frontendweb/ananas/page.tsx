e92ddbd8f090af4170ba531bf6781f119e04bdf2
duongminhthuan2003 <thuan.duongminh2003@gmail.com>
2025-09-02 21:55:43 +0700

    fix the arrangement of project detail for mobile

--- app/works/frontendweb/ananas/page.tsx
+++ app/works/frontendweb/ananas/page.tsx
@@ -5,7 +5,7 @@ import {useRouter} from "next/navigation";
 import AppleGaramond from "next/font/local";
 import SFProDisplayLight from "next/font/local";
 import SFProDisplayMedium from "next/font/local";
-import Button from "../../../ui/button"
+import Button from "../../../ui/gallerybutton"
 import ananas1 from "@/public/images/projects/ananas/overview1.png";
 import ananas2 from "@/public/images/projects/ananas/overview2.webp";
 import ananas3 from "@/public/images/projects/ananas/overview3.webp";
@@ -43,10 +43,10 @@ export default function Ananas() {
     const myRouter = useRouter();
 
     return (
-        <div className="flex flex-col h-screen mx-6 py-6 gap-2">
+        <div className="flex flex-col h-screen mx-4 py-4 gap-2">
             <div className="flex flex-row w-full justify-center items-center">
                 <div
-                    className={`${sfMono.className} flex justify-center items-center gap-4 cursor-pointer`}
+                    className={`${sfMono.className} flex justify-center items-center gap-4 cursor-pointer text-sm`}
                     onClick={() => {
                         myRouter.back();
                     }}
@@ -57,11 +57,11 @@ export default function Ananas() {
                     <p>BACK</p>
                 </div>
 
-                <div className={`${sfProDisplayLight.className} flex flex-col items-end w-full`}>
+                <div className={`${sfProDisplayLight.className} flex text-sm flex-col items-end w-full`}>
                     <p>
                         Mobile Software
                     </p>
-                    <p className={`${appleGaramond.className} text-3xl -mt-2 text-[#BA0000]`}>
+                    <p className={`${appleGaramond.className} text-2xl -mt-2 text-[#BA0000]`}>
                         zeno
                     </p>
                 </div>
@@ -77,11 +77,11 @@ export default function Ananas() {
                     initial={{opacity:0, y:20}}
                     animate={{opacity:1, y:0}}
                 >
-                    <Image src={ananas1} alt="Ananas 1" className="object-cover h-24 object-bottom w-10/12 mx-auto" />
+                    <Image src={ananas1} alt="Ananas 1" className="object-cover h-18 object-bottom w-10/12 mx-auto" />
                 </motion.div>
 
                 <motion.div
-                    className="flex-1 bg-white/30 rounded-xl border-gray-200 border-2"
+                    className="bg-white/30 rounded-xl border-gray-200 border-2 h-[120px] flex"
                     style={{
                         boxShadow: "0 2px 10px 0 rgba(0, 0, 0, 0.15)",
                         backdropFilter: "blur(4px)",
@@ -90,12 +90,12 @@ export default function Ananas() {
                     animate={{opacity:1, y:0}}
                     transition={{delay: 0.1}}
                 >
-                    <Image src={ananas2} alt="Ananas 2" className="object-cover h-full scale-95 mx-auto" />
+                    <Image src={ananas2} alt="Ananas 2" className="object-cover scale-80 mx-auto" />
                 </motion.div>
 
                 <div className="flex-1 flex flex-row gap-2">
                     <motion.div
-                        className="flex-3 bg-white/30 rounded-xl border-gray-200 border-2"
+                        className="flex-3 bg-white/30 rounded-xl border-gray-200 border-2 h-10/12"
                         style={{
                             boxShadow: "0 2px 10px 0 rgba(0, 0, 0, 0.15)",
                             backdropFilter: "blur(4px)",
@@ -104,11 +104,11 @@ export default function Ananas() {
                         animate={{opacity:1, y:0}}
                         transition={{delay: 0.2}}
                     >
-                        <Image src={ananas3} alt="Ananas 3" className="object-cover h-full scale-95 mx-auto" />
+                        <Image src={ananas3} alt="Ananas 3" className="h-full object-scale-down mx-auto" />
                     </motion.div>
 
                     <motion.div
-                        className="flex-2 bg-white/30 rounded-xl border-gray-200 border-2"
+                        className="flex-2 bg-white/30 rounded-xl border-gray-200 border-2 h-10/12"
                         style={{
                             boxShadow: "0 2px 10px 0 rgba(0, 0, 0, 0.15)",
                             backdropFilter: "blur(4px)",
@@ -123,7 +123,7 @@ export default function Ananas() {
                 </div>
 
                 <motion.div
-                    className="flex-1 bg-white/30 rounded-xl border-gray-200 border-2 px-4 h-full flex flex-col"
+                    className="flex-1 bg-white/30 rounded-xl border-gray-200 border-2 px-4 h-full flex flex-col -mt-5"
                     style={{
                         boxShadow: "0 2px 10px 0 rgba(0, 0, 0, 0.15)",
                         backdropFilter: "blur(4px)",
@@ -132,13 +132,12 @@ export default function Ananas() {
                     animate={{opacity:1, y:0}}
                     transition={{delay: 0.4}}
                 >
-                    <p className={`${sfProDisplayLight.className} flex flex-col items-end w-full text-sm mt-3`}>
-                        zeno – A React Native app that connects to an ESP32-based  smartwatch, built for my Logic Design
-                        Project course. I handled  the entire app development, from UI/UX design to coding and  Firebase
-                        integration.
+                    <p className={`${sfProDisplayLight.className} flex flex-col items-end w-full text-[13px] mt-3`}>
+                        A React Native app connecting to an ESP32-based  smart watch, built for my Logic Design
+                        Project course. I handled  the entire app development, from UI/UX design to coding.
                     </p>
                     <div className="flex-1"></div>
-                    <p className="text-sm mb-3">
+                    <p className=" text-[13px] mb-3">
                         <span className={`${sfProDisplayMedium.className}`}>Tech stack: </span><span className={`${sfProDisplayLight.className}`}>React Native, Firebase, Figma</span>
                     </p>
                 </motion.div>
@@ -149,7 +148,7 @@ export default function Ananas() {
                     animate={{opacity:1, y:0}}
                     transition={{delay: 0.5}}
                 >
-                    <div className="mt-2">
+                    <div>
                         <Button />
                     </div>
                 </motion.div>
