0888e564e8ba7540fba44eeefd3d8c60c3a72c45
duongminhthuan2003 <thuan.duongminh2003@gmail.com>
2025-09-23 09:47:48 +0700

    update page.tsx

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -1,23 +1,11 @@
 "use client"
 
-import TP1 from "../../public/images/education_TP/1.jpg"
-import TP2 from "../../public/images/education_TP/2.jpg"
-import TP3 from "../../public/images/education_TP/3.jpg"
-import TPsticker from "../../public/images/education_TP/tp_sticker.png"
-
-import BK1 from "../../public/images/education_BK/1.jpg"
-import BK2 from "../../public/images/education_BK/2.jpg"
-import BK3 from "../../public/images/education_BK/3.jpg"
-import BKsticker from "../../public/images/education_BK/bk_sticker.png"
-
 import { motion } from "framer-motion";
 import SFProDisplayLight from 'next/font/local';
 import AppleGaramondItalic from "next/font/local";
 import AboutMeFolder from "@/app/ui/aboutmefolder";
 import Image from "next/image";
 import {useState, Suspense} from "react";
-import TPPopup from "@/app/ui/aboutmepopup/education-TPpopup";
-import {AnimatePresence} from "motion/react";
 import AboutMeTimeLine from "@/app/ui/aboutmetimeline";
 
 const sfProDisplayLight = SFProDisplayLight({
@@ -41,6 +29,15 @@ function AboutMe() {
 
             <div className="h-screen w-full">
                 <p className="mt-5">Education</p>
+                <AboutMeFolder
+                    images={[
+                        {src: "edu-tp-1", alt: "test"},
+                        {src: "edu-tp-2", alt: "test"},
+                        {src: "edu-tp-3", alt: "test"}
+                    ]}
+                    title={"Tan Phu High School"}
+                />
+
                 <AboutMeFolder
                     images={[
                         {src: "edu-tp-1", alt: "test"},
