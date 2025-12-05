2f09b23c4820283cf88ce69be8206b13f254353b
duongminhthuan2003 <thuan.duongminh2003@gmail.com>
2025-12-05 21:19:47 +0700

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -15,58 +15,64 @@ const appleGaramond = AppleGaramond({
 export default function Home() {
 
     return (
-          <motion.div
-              className={`antialiased min-h-[100dvh] flex items-center justify-center w-full overflow-x-hidden scrollbar-hide`}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.3, ease: "easeInOut" }}
-              key="home-page"
-          >
-              <motion.div
-                  className={`${appleGaramond.className} flex flex-col items-center gap-6 h-full justify-center -translate-y-6`}
-                  initial={{ opacity: 0, y: 20 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 0.4, ease: "easeInOut", delay: 0.1 }}
-              >
-                  <motion.div
-                      className="relative w-[190px] aspect-[2/3] -rotate-[5deg]"
-                      initial={{ opacity: 0, transform: "translateY(20px)" }}
-                      animate={{ opacity: 1, transform: "translateY(0px)" }}
-                      transition={{ duration: 0.3, ease: "easeInOut", delay: 0.3 }}
-                  >
-                      <Image
-                          src="/images/HomeImage.jpg"
-                          alt="Home image"
-                          fill
-                          className="object-cover object-center"
-                          style={{boxShadow: "0 3px 6px 0 rgba(0, 0, 0, 0.25), 0 4px 15px 0 rgba(0, 0, 0, 0.25)"}}
-                          priority
-                      />
-                      <div className="absolute rotate-5 h-5 top-36 -left-2 aspect-square bg-[#BA0000] z-10"/>
-                      <div className="absolute rotate-5 h-5 -bottom-2 -right-2 aspect-square bg-[#BA0000] z-10"/>
-                      <div className="absolute rotate-5 h-5 top-10 -right-6 aspect-[3/1] bg-[#BA0000] z-10"/>
-                  </motion.div>
+        <div className='scroll-snap overflow-hidden scrollbar-hide'>
+            <motion.div
+                className={`antialiased min-h-[100dvh] flex flex-col items-center justify-center overflow-hidden scrollbar-hide`}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+                key="home-page"
+            >
+                <motion.div
+                    className={`${appleGaramond.className} flex flex-col items-center gap-6 h-full justify-center -translate-y-6`}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.4, ease: "easeInOut", delay: 0.1 }}
+                >
+                    <motion.div
+                        className="relative w-[190px] aspect-[2/3] -rotate-[5deg]"
+                        initial={{ opacity: 0, transform: "translateY(20px)" }}
+                        animate={{ opacity: 1, transform: "translateY(0px)" }}
+                        transition={{ duration: 0.3, ease: "easeInOut", delay: 0.3 }}
+                    >
+                        <Image
+                            src="/images/HomeImage.jpg"
+                            alt="Home image"
+                            fill
+                            className="object-cover object-center"
+                            style={{boxShadow: "0 3px 6px 0 rgba(0, 0, 0, 0.25), 0 4px 15px 0 rgba(0, 0, 0, 0.25)"}}
+                            priority
+                        />
+                        <div className="absolute rotate-5 h-5 top-36 -left-2 aspect-square bg-[#BA0000] z-10"/>
+                        <div className="absolute rotate-5 h-5 -bottom-2 -right-2 aspect-square bg-[#BA0000] z-10"/>
+                        <div className="absolute rotate-5 h-5 top-10 -right-6 aspect-[3/1] bg-[#BA0000] z-10"/>
+                    </motion.div>
 
-                  <div className="text-[28px] text-center mx-6 -mb-4">
-                      <SplitText
-                          text="I am an interface designer, a web builder, a software developer, and sometimes a photographer."
-                          splitType="words"
-                          highlightWords={['designer', 'builder', 'developer', 'photographer']}
-                          delay={150}
-                          className="text-2xl"
-                      />
-                  </div>
-                  <motion.div
-                      className="w-fit h-fit"
-                      initial={{ opacity: 0, transform: "translateY(20px)" }}
-                      animate={{ opacity: 1, transform: "translateY(0px)" }}
-                      exit={{opacity: 0, transform: "translateX(-60px)" }}
-                      transition={{ duration: 0.3, ease: "easeInOut", delay: 0.3 }}
-                  >
-                      <Button />
-                  </motion.div>
-              </motion.div>
-          </motion.div>
+                    <div className="text-[28px] text-center mx-6 -mb-4">
+                        <SplitText
+                            text="I am an interface designer, a web builder, a software developer, and sometimes a photographer."
+                            splitType="words"
+                            highlightWords={['designer', 'builder', 'developer', 'photographer']}
+                            delay={150}
+                            className="text-2xl"
+                        />
+                    </div>
+                    <motion.div
+                        className="w-fit h-fit"
+                        initial={{ opacity: 0, transform: "translateY(20px)" }}
+                        animate={{ opacity: 1, transform: "translateY(0px)" }}
+                        exit={{opacity: 0, transform: "translateX(-60px)" }}
+                        transition={{ duration: 0.3, ease: "easeInOut", delay: 0.3 }}
+                    >
+                        <Button />
+                    </motion.div>
+                </motion.div>
+            </motion.div>
+
+            <div className="h-screen w-full overflow-hidden">
+                <p>test</p>
+            </div>
+        </div>
   );
 }
