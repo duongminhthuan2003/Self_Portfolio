8e41e9c962c3fbdad3542ba939d0a1269fb7084e
duongminhthuan2003 <thuan.duongminh2003@gmail.com>
2025-12-05 11:59:50 +0700

    update tp popup.tsx

--- app/ui/aboutmepopup/education-TPpopup.tsx
+++ app/ui/aboutmepopup/education-TPpopup.tsx
@@ -5,7 +5,6 @@ import SFProDisplayMedium from "next/font/local";
 import {CldImage} from "next-cloudinary";
 import { HugeiconsIcon } from '@hugeicons/react';
 import { Cancel01Icon } from '@hugeicons/core-free-icons';
-import { linear } from "maath/dist/declarations/src/easing";
 
 function App() {
   return (
@@ -17,6 +16,7 @@ function App() {
     />
   );
 }
+
 const sfProDisplayLight = SFProDisplayLight({
     weight:"200",
     src: "../../../public/fonts/SFProDisplay-Light.otf",
@@ -32,92 +32,91 @@ const sfProDisplayMedium = SFProDisplayMedium({
 function TPPopup({ onClose }: { onClose: () => void }) {
     return(
         <motion.div
-            className="absolute top-0 left-0 w-full h-full bg-white/50 z-[70] backdrop-blur-lg"
+            className="fixed inset-0 w-full h-screen bg-white/50 z-[70] backdrop-blur-lg overflow-hidden"
             initial={{opacity: 0}}
             animate={{opacity: 1}}
             exit={{opacity: 0}}
-
             transition={{ease: "easeInOut", duration: 0.2}}
         >
             <div
-                className="w-[300px] h-full mx-auto relative"
+                className="w-[300px] h-full max-h-screen mx-auto relative flex items-center justify-center py-4"
             >
-                <motion.div
-                    initial={{scale: 0.9, opacity: 0}}
-                    animate={{scale: 1, opacity: 1}}
-                    exit={{scale: 0.9, opacity: 0}}
-                    transition={{ease: "easeInOut", duration: 0.2}}
-                    className="absolute top-[6%]"
-                >
-                    <CldImage src="edu-tp-1" height={150} width={150} alt={"Tan Phu 1"} className={`rounded-lg`}/>
-                </motion.div>
+                <div className="relative w-full h-[600px] max-h-[85vh]">
+                    <motion.div
+                        initial={{scale: 0.9, opacity: 0}}
+                        animate={{scale: 1, opacity: 1}}
+                        exit={{scale: 0.9, opacity: 0}}
+                        transition={{ease: "easeInOut", duration: 0.2}}
+                        className="absolute top-[2%]"
+                    >
+                        <CldImage src="edu-tp-1" height={120} width={120} alt={"Tan Phu 1"} className={`rounded-lg`}/>
+                    </motion.div>
 
-                <motion.div
-                    initial={{scale: 0.9, opacity: 0}}
-                    animate={{scale: 1, opacity: 1}}
-                    exit={{scale: 0.9, opacity: 0}}
-                    transition={{ease: "easeInOut", duration: 0.2, delay: 0.1}}
-                    className="absolute top-[11%] right-0"
-                >
-                    <CldImage src="edu-tp-2" height={175} width={175} alt={"Tan Phu 2"} className="rounded-lg"/>
-                </motion.div>
+                    <motion.div
+                        initial={{scale: 0.9, opacity: 0}}
+                        animate={{scale: 1, opacity: 1}}
+                        exit={{scale: 0.9, opacity: 0}}
+                        transition={{ease: "easeInOut", duration: 0.2, delay: 0.1}}
+                        className="absolute top-[8%] right-0"
+                    >
+                        <CldImage src="edu-tp-2" height={150} width={150} alt={"Tan Phu 2"} className="rounded-lg"/>
+                    </motion.div>
 
-                <motion.div
-                    initial={{scale: 0.9, opacity: 0}}
-                    animate={{scale: 1, opacity: 1}}
-                    exit={{scale: 0.9, opacity: 0}}
-                    transition={{ease: "easeInOut", duration: 0.2, delay: 0.2}}
-                    className={`flex absolute top-[47%] h-1/5 items-center left-0 transform -translate-y-1/2 bg-white px-5 py-4 rounded-xl w-full`}
-                    style={{
-                        boxShadow: '0 2px 10px 0 rgba(0, 0, 0, 0.15)'
-                    }}
-                >
-                    <div>
-                        <p className={`${sfProDisplayMedium.className}`}>Tan Phu Highschool</p>
-                        <p className={`${sfProDisplayLight.className}`}>GPA: 8.5/10</p>
-                        <p className={`${sfProDisplayLight.className}`}>2018 - 2021</p>
-                    </div>
-                   <CldImage src="tp_sticker" height={125} width={125} alt={"Tan Phu Stickers"} className="absolute top-[45%] -translate-y-1/2 right-0"/>
-                </motion.div>
+                    <motion.div
+                        initial={{scale: 0.9, opacity: 0}}
+                        animate={{scale: 1, opacity: 1}}
+                        exit={{scale: 0.9, opacity: 0}}
+                        transition={{ease: "easeInOut", duration: 0.2, delay: 0.2}}
+                        className={`flex absolute top-[45%] items-center left-0 transform -translate-y-1/2 bg-white px-5 py-4 rounded-xl w-full`}
+                        style={{
+                            boxShadow: '0 2px 10px 0 rgba(0, 0, 0, 0.15)'
+                        }}
+                    >
+                        <div>
+                            <p className={`${sfProDisplayMedium.className} text-sm`}>Tan Phu Highschool</p>
+                            <p className={`${sfProDisplayLight.className} text-sm`}>GPA: 8.5/10</p>
+                            <p className={`${sfProDisplayLight.className} text-sm`}>2018 - 2021</p>
+                        </div>
+                       <CldImage src="tp_sticker" height={100} width={100} alt={"Tan Phu Stickers"} className="absolute top-[45%] -translate-y-1/2 right-0"/>
+                    </motion.div>
 
+                    <motion.div
+                        initial={{scale: 0.9, opacity: 0}}
+                        animate={{scale: 1, opacity: 1}}
+                        exit={{scale: 0.9, opacity: 0}}
+                        transition={{ease: "easeInOut", duration: 0.2, delay: 0.3}}
+                        className="absolute bottom-[16%] left-0"
+                    >
+                        <CldImage src="edu-tp-3" height={90} width={150} alt={"Tan Phu 3"} className="rounded-lg"/>
+                    </motion.div>
 
-                <motion.div
-                    initial={{scale: 0.9, opacity: 0}}
-                    animate={{scale: 1, opacity: 1}}
-                    exit={{scale: 0.9, opacity: 0}}
-                    transition={{ease: "easeInOut", duration: 0.2, delay: 0.3}}
-                    className="absolute bottom-[18%] left-0"
-                >
-                    <CldImage src="edu-tp-3" height={100} width={175} alt={"Tan Phu 3"} className="rounded-lg"/>
-                </motion.div>
+                    <motion.div
+                        initial={{scale: 0.9, opacity: 0}}
+                        animate={{scale: 1, opacity: 1}}
+                        exit={{scale: 0.9, opacity: 0}}
+                        transition={{ease: "easeInOut", duration: 0.2, delay: 0.4}}
+                        className="absolute bottom-[21%] right-0 w-fit h-fit"
+                    >
+                        <CldImage src="edu-tp-4" height={90} width={150} alt={"Tan Phu 4"} className="rounded-lg"/>
+                    </motion.div>
 
-                <motion.div
-                    initial={{scale: 0.9, opacity: 0}}
-                    animate={{scale: 1, opacity: 1}}
-                    exit={{scale: 0.9, opacity: 0}}
-                    transition={{ease: "easeInOut", duration: 0.2, delay: 0.4}}
-                    className="absolute bottom-[24%] right-0 w-fit h-fit"
-                >
-                    <CldImage src="edu-tp-4" height={100} width={175} alt={"Tan Phu 4"} className="rounded-lg"/>
-                </motion.div>
-
-                <button 
-                    onClick={onClose} 
-                    className="absolute bottom-[8%] left-1/2 transform -translate-x-1/2 bg-red-500 p-3 rounded-full z-10 border-1 border-[#DBDBDB]"
-                    style={{
-                        background: 'linear-gradient(180deg, rgba(255, 255, 255, 0.80) 8%, rgba(231, 230, 230, 0.8) 50%, rgba(233, 233, 233, 0.8) 70%, rgba(255, 255, 255, 0.80) 98.08%)',
-                        backdropFilter: 'blur(10px)',
-                        filter: 'drop-shadow(0 4px 10px rgba(0, 0, 0, 0.1))',
-                        opacity: 0.8,
-                    }}
-                >
-                    <HugeiconsIcon
-                        icon={Cancel01Icon}
-                        size={24}
-                        color="#000000"
-                        strokeWidth={1.5}
-                    />
-                </button>
+                    <button 
+                        onClick={onClose} 
+                        className="absolute bottom-[5%] left-1/2 transform -translate-x-1/2 bg-red-500 p-3 rounded-full z-10 border-1 border-[#DBDBDB] shadow-xl"
+                        style={{
+                            background: 'linear-gradient(180deg, rgba(255, 255, 255, 0.80) 8%, rgba(231, 230, 230, 0.8) 50%, rgba(233, 233, 233, 0.8) 70%, rgba(255, 255, 255, 0.80) 98.08%)',
+                            backdropFilter: 'blur(10px)',
+                            opacity: 0.8,
+                        }}
+                    >
+                        <HugeiconsIcon
+                            icon={Cancel01Icon}
+                            size={24}
+                            color="#000000"
+                            strokeWidth={1.5}
+                        />
+                    </button>
+                </div>
             </div>
             <div className="absolute top-0 left-0 w-full h-full -z-10" onClick={onClose}/>
         </motion.div>
