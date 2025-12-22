7357f2ebbc5a1e7d17250581651caccc6b524e20
duongminhthuan2003 <thuan.duongminh2003@gmail.com>
2025-12-23 01:51:23 +0700

    update front-end masonry

--- app/works/frontendweb/ananas/gallery/page.tsx
+++ app/works/frontendweb/ananas/gallery/page.tsx
@@ -89,12 +89,13 @@ export default function ZenoGallery(){
                     duration={0.6}
                     stagger={0.05}
                     animateFrom="bottom"
-                    scaleOnHover={true}
+                    scaleOnHover={false}
                     hoverScale={0.95}
                     blurToFocus={true}
                     colorShiftOnHover={false}
                 />
             </div>
+            <div className="h-6"></div>
         </div>
     )
 }
